22396ddd91a35d2b4fbd01d342b39d0532dc854d
turboNinja2 <julien.revelle@gmail.com>
2015-06-19 16:32:34 +0200

    Removed useless files and cleaned code

---
+++ NDSB/SparseDistances.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NDSB
+{
+    public static class SparseDistances
+    {
+        public static double EuclideDistance(Dictionary<string, double> sp1, Dictionary<string, double> sp2)
+        {
+            double distance = 0;
+            foreach (KeyValuePair<string, double> kvp1 in sp1)
+            {
+                if (sp2.ContainsKey(kvp1.Key))
+                    distance += Math.Pow((kvp1.Value - sp2[kvp1.Key]), 2);
+                else
+                    distance += Math.Pow((kvp1.Value), 2);
+            }
+
+            foreach (KeyValuePair<string, double> kvp2 in sp2)
+                if (!sp1.ContainsKey(kvp2.Key))
+                    distance += Math.Pow((kvp2.Value), 2);
+
+            return distance;
+        }
+
+        public static double ManhattanDistance(Dictionary<string, double> sp1, Dictionary<string, double> sp2)
+        {
+            double distance = 0;
+            foreach (KeyValuePair<string, double> kvp1 in sp1)
+            {
+                if (sp2.ContainsKey(kvp1.Key))
+                    distance += Math.Abs(kvp1.Value - sp2[kvp1.Key]);
+                else
+                    distance += Math.Abs(kvp1.Value);
+            }
+
+            foreach (KeyValuePair<string, double> kvp2 in sp2)
+                if (!sp1.ContainsKey(kvp2.Key))
+                    distance += Math.Abs(kvp2.Value);
+
+            return distance;
+        }
+    }
+}
--- NDSB/SparseKNN.cs
+++ NDSB/SparseKNN.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace NDSB
 {
-    class SparseKNN
+    public static class SparseKNN
     {
-
         private static int[] LazyBubbleSort(int[] labels, double[] distances, int elementsToSort)
         {
             int[] labelsCopy = new int[labels.Length];
@@ -21,9 +20,9 @@ namespace NDSB
                 for (int i = n - 2; i >= 0; i--)
                     if (distancesCopy[i] > distancesCopy[i + 1])
                     {
-                        double tmp = distancesCopy[i + 1];
+                        double distanceTmp = distancesCopy[i + 1];
                         distancesCopy[i + 1] = distancesCopy[i];
-                        distancesCopy[i] = tmp;
+                        distancesCopy[i] = distanceTmp;
 
                         int labelTmp = labelsCopy[i + 1];
                         labelsCopy[i + 1] = labelsCopy[i];
@@ -35,60 +34,20 @@ namespace NDSB
             return result;
         }
 
-        private static double EuclideDistance(Dictionary<string, double> sp1, Dictionary<string, double> sp2)
-        {
-            double distance = 0;
-            foreach (KeyValuePair<string, double> kvp1 in sp1)
-            {
-                if (sp2.ContainsKey(kvp1.Key))
-                    distance += Math.Pow((kvp1.Value - sp2[kvp1.Key]), 2);
-                else
-                    distance += Math.Pow((kvp1.Value), 2);
-            }
-
-            foreach (KeyValuePair<string, double> kvp2 in sp2)
-                if (!sp1.ContainsKey(kvp2.Key))
-                    distance += Math.Pow((kvp2.Value), 2);
-
-            return distance;
-        }
-
         public static Dictionary<string, double> ToCube(Dictionary<string, double> input)
         {
             Dictionary<string, double> res = new Dictionary<string, double>();
             if (input.Count() == 0) return res;
-            
             double max = input.Select(kvp => kvp.Value).Max();
-                        foreach (KeyValuePair<string, double> kvp in input)
+            foreach (KeyValuePair<string, double> kvp in input)
                 res.Add(kvp.Key, kvp.Value / max);
             return res;
         }
 
-        private static double ManhattanDistance(Dictionary<string, double> sp1, Dictionary<string, double> sp2)
-        {
-            double distance = 0;
-            foreach (KeyValuePair<string, double> kvp1 in sp1)
-            {
-                if (sp2.ContainsKey(kvp1.Key))
-                    distance += Math.Abs(kvp1.Value - sp2[kvp1.Key]);
-                else
-                    distance += Math.Abs(kvp1.Value);
-            }
-
-            foreach (KeyValuePair<string, double> kvp2 in sp2)
-                if (!sp1.ContainsKey(kvp2.Key))
-                    distance += Math.Abs(kvp2.Value);
-
-            return distance;
-        }
-
-
         public static int Predict(int[] labels, Dictionary<string, double>[] sample, Dictionary<string, double> newPoint, int nbNeighbours)
         {
             double[] distances = new double[sample.Length];
-
-            Parallel.For(0, sample.Length, i => { distances[i] = ManhattanDistance(newPoint, sample[i]); });
-
+            Parallel.For(0, sample.Length, i => { distances[i] = SparseDistances.ManhattanDistance(newPoint, sample[i]); });
             int[] neighboursLabels = LazyBubbleSort(labels, distances, nbNeighbours);
             int mostLikely = neighboursLabels.GroupBy(i => i).OrderByDescending(grp => grp.Count()).Select(grp => grp.Key).First();
             return mostLikely;
