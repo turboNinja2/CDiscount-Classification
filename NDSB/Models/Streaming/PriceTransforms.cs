e711923c161a65184922dea72ab9b565351df4f5
turboNinja2 <julien.revelle@gmail.com>
2015-07-21 19:55:51 +0200

    Streaming (part I)

---
+++ NDSB/Models/Streaming/PriceTransforms.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataScienceECom.Phis
+{
+    public static class PriceTransforms
+    {
+        public static int LogPrice(double price)
+        {
+            if (price > 0)
+                return Convert.ToInt32(Math.Log(price));
+            else
+                return -1;
+        }
+
+        public static int Log10Price(double price)
+        {
+            if (price > 0)
+                return Convert.ToInt32(Math.Log10(price));
+            else
+                return -1;
+        }
+    }
+}
---
+++ NDSB/Models/Streaming/StringCleaner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DataScienceECom.Phis
+{
+    public static class StringCleaner
+    {
+        public static string None(string input)
+        {
+            return input;
+        }
+
+        public static string RemoveSomePunctuation(string input)
+        {
+            input = input.Replace(":", "");
+            input = input.Replace(".", "");
+            return input;
+        }
+
+        public static string RemoveSomePunctuationAndAccents(string input)
+        {
+            input = input.Replace(":", "");
+            input = input.Replace(".", "");
+
+            input = input.Replace("é", "e");
+            input = input.Replace("è", "e");
+            input = input.Replace("ê", "e");
+            input = input.Replace("à", "a");
+
+            return input;
+        }
+
+
+        public static string RemoveMorePunctuationAndAccents(string input)
+        {
+            input = RemoveSomePunctuationAndAccents(input);
+
+            input = input.Replace("?", "");
+            input = input.Replace("!", "");
+            input = input.Replace("*", "");
+
+            return input;
+        }
+
+
+        public static string RemoveMorePunctuationAndAccents2(string input)
+        {
+            input = RemoveSomePunctuationAndAccents(input);
+
+            input = input.Replace("?", "");
+            input = input.Replace("!", "");
+            input = input.Replace("*", "");
+            input = input.Replace(".", "");
+
+            return input;
+        }
+
+        public static string RemoveMorePunctuationAndAccents3(string input)
+        {
+            input = RemoveMorePunctuationAndAccents2(input);
+
+            input = input.Replace(")", "");
+            input = input.Replace("(", "");
+            input = input.Replace("\"", "");
+            input = input.Replace(",", "");
+            input = Regex.Replace(input, "<br*>", "");
+
+            return input;
+        }
+
+    }
+}
