fdead40c4c9049ac3929391a30793a867b6cceb8
turboNinja2 <julien.revelle@gmail.com>
2015-08-09 22:53:08 +0200

    Not much

--- NDSB/Models/Streaming/ModelGenerators.cs
+++ NDSB/Models/Streaming/ModelGenerators.cs
@@ -9,6 +9,11 @@ namespace DataScienceECom.Models
         public static IEnumerable<IStreamingModel<int,int>> Entropia6()
         {
             yield return new GMEntropia(0.5, 4, 6, 500000);
+            yield return new GMEntropia(0.6, 4, 6, 500000);
+            yield return new GMEntropia(0.7, 4, 6, 500000);
+            yield return new GMEntropia(0.5, 4, 7, 500000);
+            yield return new GMEntropia(0.6, 4, 7, 500000);
+            yield return new GMEntropia(0.7, 4, 7, 500000);
         }
 
         public static IEnumerable<IStreamingModel<Hierarchy, int>> HEntropia()
