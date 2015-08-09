bd1506c71d873fbdd4c1d8a3c80fcf54922f74a6
turboNinja2 <julien.revelle@gmail.com>
2015-08-09 21:18:53 +0200

    Removed unused methods

--- NDSB/TextUtils/TFIDF.cs
+++ NDSB/TextUtils/TFIDF.cs
@@ -148,9 +148,7 @@ namespace NDSB
                 });
 
                 vectors.Add(vector.ToList());
-
             }
-
             return vectors.Select(v => v.ToArray()).ToArray();
         }
 
@@ -182,48 +180,6 @@ namespace NDSB
                 }
                 yield return sparseLine;
             }
-        }
-
-        /// <summary>
-        /// Normalizes a TF*IDF array of vectors using L2-Norm.
-        /// Xi = Xi / Sqrt(X0^2 + X1^2 + .. + Xn^2)
-        /// </summary>
-        /// <param name="vectors">double[][]</param>
-        /// <returns>double[][]</returns>
-        public static double[][] Normalize(double[][] vectors)
-        {
-            // Normalize the vectors using L2-Norm.
-            List<double[]> normalizedVectors = new List<double[]>();
-            foreach (var vector in vectors)
-            {
-                var normalized = Normalize(vector);
-                normalizedVectors.Add(normalized);
-            }
-
-            return normalizedVectors.ToArray();
-        }
-
-        /// <summary>
-        /// Normalizes a TF*IDF vector using L2-Norm.
-        /// Xi = Xi / Sqrt(X0^2 + X1^2 + .. + Xn^2)
-        /// </summary>
-        /// <param name="vectors">double[][]</param>
-        /// <returns>double[][]</returns>
-        public static double[] Normalize(double[] vector)
-        {
-            List<double> result = new List<double>();
-
-            double sumSquared = 0;
-            foreach (var value in vector)
-                sumSquared += value * value;
-
-            double SqrtSumSquared = Math.Sqrt(sumSquared);
-
-            // L2-norm: Xi = Xi / Sqrt(X0^2 + X1^2 + .. + Xn^2)
-            foreach (var value in vector)
-                result.Add(value / SqrtSumSquared);
-
-            return result.ToArray();
         }
 
         /// <summary>
