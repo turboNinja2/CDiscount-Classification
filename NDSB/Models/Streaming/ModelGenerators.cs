407ee42fc012999f6141c0bef057eae02849f833
turboNinja2 <julien.revelle@gmail.com>
2015-08-11 23:27:34 +0200

    Nothing important

--- NDSB/Models/Streaming/ModelGenerators.cs
+++ NDSB/Models/Streaming/ModelGenerators.cs
@@ -18,13 +18,7 @@ namespace DataScienceECom.Models
 
         public static IEnumerable<IStreamingModel<Hierarchy, int>> HEntropia()
         {
-            yield return new HierarchicalSGD(0.5, 4, 8, 1000);
-            yield return new HierarchicalSGD(0.5, 4, 8);
-            yield return new HierarchicalSGD(0.7, 4, 8);
-            yield return new HierarchicalSGD(0.9, 4, 8);
             yield return new HierarchicalSGD(0.5, 4, 6);
-            yield return new HierarchicalSGD(0.7, 4, 6);
-            yield return new HierarchicalSGD(0.9, 4, 6);
         }
 
     }
